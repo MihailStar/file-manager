2bb1e73a8d36f3d17756558e94aabeedefc0969e
MihailStar <mhlstr@mail.ru>
2022-12-08 18:15:13 +0300

    feat: add parsing of input

--- src/handle-input.js
+++ src/handle-input.js
@@ -1,3 +1,24 @@
-async function handleInput(input) {}
+/** @tutorial https://qna.habr.com/answer?answer_id=1587593 */
+const partOfCommandRegExp = /"([^"]+)"|([^ ]+)/g;
+
+async function handleInput(trimedInput) {
+  if (trimedInput.length === 0) {
+    return;
+  }
+
+  const matches = trimedInput.matchAll(partOfCommandRegExp);
+  const сommandParts = [];
+
+  for (const match of matches) {
+    const [, partOfCommandWithQuotes, partOfCommandWithoutQuotes] = match;
+    const partOfCommand = partOfCommandWithQuotes ?? partOfCommandWithoutQuotes;
+
+    сommandParts.push(partOfCommand);
+  }
+
+  const [commandName, ...commandArgs] = сommandParts;
+
+  console.log({ commandName, commandArgs });
+}
 
 export { handleInput };
