f96d64a46c2efc4c82c230b02526b0246d0b96cb
MihailStar <mhlstr@mail.ru>
2022-12-08 18:51:04 +0300

    refactor

--- src/handle-input.js
+++ src/handle-input.js
@@ -1,4 +1,5 @@
 import { InputError } from './error/input-error.js';
+import { OperationError } from './error/operation-error.js';
 
 const partOfCommandRegExp = /"([^"]+)"|([^ ]+)/g;
 
@@ -22,13 +23,22 @@ function parseInput(trimedInput) {
   return { commandName, commandArgs };
 }
 
+function handleError(reason) {
+  if (reason instanceof InputError || reason instanceof OperationError) {
+    console.error(reason.message);
+    return;
+  }
+
+  console.error(new OperationError().message);
+}
+
 async function handleInput(trimedInput) {
   try {
     const { commandName, commandArgs } = parseInput(trimedInput);
 
     console.log({ commandName, commandArgs });
   } catch (reason) {
-    console.error(reason);
+    handleError(reason);
   }
 }
 
