713b5cf22b91b0f357e8321af728653cc0b98776
MihailStar <mhlstr@mail.ru>
2022-12-11 17:44:57 +0300

    feat: add 'decompress' command

--- src/executor/decompress-executor.js
+++ src/executor/decompress-executor.js
@@ -1,11 +1,28 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { basename, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { createBrotliDecompress } from 'zlib';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
+const compressorExt = '.br';
+
 class DecompressExecutor extends AbstractExecutor {
-  async executor(filePath, newFilePath) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+  async executor(filePath, dirPath) {
+    const inputFileName = basename(filePath);
+    const inputFilePath = getAbsolutePath(filePath);
+
+    const outputDirPath = getAbsolutePath(dirPath);
+    const outputFileName = inputFileName.endsWith(compressorExt)
+      ? inputFileName.slice(0, -compressorExt.length)
+      : inputFileName;
+    const outputFilePath = join(outputDirPath, outputFileName);
+
+    const readStream = createReadStream(inputFilePath);
+    const transformStream = createBrotliDecompress();
+    const writeStream = createWriteStream(outputFilePath);
+
+    await pipeline(readStream, transformStream, writeStream);
   }
 }
 
