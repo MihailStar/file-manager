7e7f93ab960cee5eb2c0be7b85da8652e048e0fe
MihailStar <mhlstr@mail.ru>
2022-12-11 17:44:19 +0300

    feat: add 'compress' command

--- src/executor/compress-executor.js
+++ src/executor/compress-executor.js
@@ -1,11 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { basename, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { createBrotliCompress } from 'zlib';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
+const compressorExt = '.br';
+
 class CompressExecutor extends AbstractExecutor {
-  async executor(filePath, newFilePath) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+  async executor(filePath, dirPath) {
+    const inputFileName = basename(filePath);
+    const inputFilePath = getAbsolutePath(filePath);
+
+    const outputDirPath = getAbsolutePath(dirPath);
+    const outputFileName = `${inputFileName}${compressorExt}`;
+    const outputFilePath = join(outputDirPath, outputFileName);
+
+    const readStream = createReadStream(inputFilePath);
+    const transformStream = createBrotliCompress();
+    const writeStream = createWriteStream(outputFilePath);
+
+    await pipeline(readStream, transformStream, writeStream);
   }
 }
 
