b28935508e316175354d386cf4ebce8feb475760
MihailStar <mhlstr@mail.ru>
2022-12-11 22:41:26 +0300

    feat: add 'rm' command

--- src/executor/rm-executor.js
+++ src/executor/rm-executor.js
@@ -1,11 +1,12 @@
+import { unlink } from 'fs/promises';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
 class RmExecutor extends AbstractExecutor {
   async executor(filePath) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+    const inputFilePath = getAbsolutePath(filePath);
+
+    await unlink(inputFilePath);
   }
 }
 
