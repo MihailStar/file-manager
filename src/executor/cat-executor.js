9b5bea9abd9737b0b9676960d9542592d291fd49
MihailStar <mhlstr@mail.ru>
2022-12-11 18:12:08 +0300

    feat: add 'cat' command

--- src/executor/cat-executor.js
+++ src/executor/cat-executor.js
@@ -1,11 +1,24 @@
+import { once } from 'events';
+import { createReadStream } from 'fs';
+import { EOL } from 'os';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
 class CatExecutor extends AbstractExecutor {
   async executor(filePath) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+    const inputFilePath = getAbsolutePath(filePath);
+    const readStream = createReadStream(inputFilePath);
+    const writeStream = process.stdout;
+
+    readStream
+      .on('end', () => {
+        writeStream.write(EOL);
+      })
+      .on('data', (chunk) => {
+        writeStream.write(chunk);
+      });
+
+    await once(readStream, 'close');
   }
 }
 
