5aed8f9ce562d9e92dbda690dd8ca86fed11d2ad
MihailStar <mhlstr@mail.ru>
2022-12-11 22:37:20 +0300

    refactor

--- src/executor/rn-executor.js
+++ src/executor/rn-executor.js
@@ -1,15 +1,20 @@
 import { rename } from 'fs/promises';
 import { OperationError } from '../error/operation-error.js';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { isExist } from '../utility/is-exist.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
 class RnExecutor extends AbstractExecutor {
   async executor(filePath, newFilePath) {
-    if (await isExist(newFilePath)) {
+    const inputFilePath = getAbsolutePath(filePath);
+    const outputFilePath = getAbsolutePath(newFilePath);
+
+    // rewrite to `isExistSync`
+    if (await isExist(outputFilePath)) {
       throw new OperationError('Code EEXIST');
     }
 
-    await rename(filePath, newFilePath);
+    await rename(inputFilePath, outputFilePath);
   }
 }
 
