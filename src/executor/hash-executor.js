468317e5a6bcefdc1e94b7b95291b7fae3bbc576
MihailStar <mhlstr@mail.ru>
2022-12-11 16:50:12 +0300

    feat: add 'hash' command

--- src/executor/hash-executor.js
+++ src/executor/hash-executor.js
@@ -1,11 +1,18 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
 class HashExecutor extends AbstractExecutor {
   async executor(filePath) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+    const absolutePath = getAbsolutePath(filePath);
+    const readStream = createReadStream(absolutePath);
+    const transformStream = createHash('sha256');
+
+    await pipeline(readStream, transformStream);
+
+    console.log(transformStream.digest('hex'));
   }
 }
 
