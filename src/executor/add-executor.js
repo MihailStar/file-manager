fec05c60e29fe38ed79690a5953d0e49140bc71f
MihailStar <mhlstr@mail.ru>
2022-12-11 21:03:31 +0300

    refactor

--- src/executor/add-executor.js
+++ src/executor/add-executor.js
@@ -1,9 +1,11 @@
 import { open } from 'fs/promises';
+import { getAbsolutePath } from '../utility/get-absolute-path.js';
 import { AbstractExecutor } from './abstract-executor.js';
 
 class AddExecutor extends AbstractExecutor {
   async executor(filePath) {
-    const fileHandle = await open(filePath, 'wx');
+    const inputFilePath = getAbsolutePath(filePath);
+    const fileHandle = await open(inputFilePath, 'wx');
 
     await fileHandle.close();
   }
