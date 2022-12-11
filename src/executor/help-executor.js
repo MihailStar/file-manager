ec6094efe6926873b4d703aa1acd15928e09bc74
MihailStar <mhlstr@mail.ru>
2022-12-11 14:48:40 +0300

    feat: add 'help' command

--- src/executor/help-executor.js
+++ src/executor/help-executor.js
@@ -2,10 +2,33 @@ import { AbstractExecutor } from './abstract-executor.js';
 
 class HelpExecutor extends AbstractExecutor {
   async executor() {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+    const commandList = `
+up
+cd <directory_path>
+ls
+
+cat <file_path>
+add <file_path>
+rn <file_path> <file_path>
+cp <file_path> <directory_path>
+mv <file_path> <directory_path>
+rm <file_path>
+
+os --EOL
+os --cpus
+os --homedir
+os --username
+os --architecture
+
+hash <file_path>
+
+compress <file_path> <file_path>
+decompress <file_path> <file_path>
+
+.exit
+`;
+
+    console.log(commandList.trim());
   }
 }
 
