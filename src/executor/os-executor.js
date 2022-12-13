55b4d98a4ba2d03099c034111b1821913d91f15d
MihailStar <mhlstr@mail.ru>
2022-12-13 20:04:30 +0300

    feat: add 'os' command

--- src/executor/os-executor.js
+++ src/executor/os-executor.js
@@ -1,11 +1,57 @@
+import { arch, cpus, EOL, homedir, userInfo } from 'os';
 import { AbstractExecutor } from './abstract-executor.js';
 
+function getEOL() {
+  console.log(JSON.stringify(EOL).slice(1, -1));
+}
+
+function getCpus() {
+  console.table(
+    cpus().map(({ model, speed: speedInMHz }) => ({
+      model,
+      speedInGHz: speedInMHz / 1000,
+    }))
+  );
+}
+
+function getHomedir() {
+  console.log(homedir());
+}
+
+function getUsername() {
+  try {
+    console.log(userInfo().username);
+  } catch {
+    console.log('Unknown');
+  }
+}
+
+function getArchitecture() {
+  console.log(arch());
+}
+
+const optionToFunction = {
+  '--EOL': getEOL,
+  '--cpus': getCpus,
+  '--homedir': getHomedir,
+  '--username': getUsername,
+  '--architecture': getArchitecture,
+};
+
 class OsExecutor extends AbstractExecutor {
+  validateArgs(...args) {
+    if (
+      super.validateArgs(...args) &&
+      args.every((arg) => typeof optionToFunction[arg] === 'function')
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
   async executor(option) {
-    console.log({
-      commandExecutor: this.constructor.name,
-      commandArgs: arguments,
-    });
+    optionToFunction[option]();
   }
 }
 
